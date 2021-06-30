ee339a6a27b27c2ee27cc67e5cfc6cafaf0f4c0a
Ahouans84 <79781174+Ahouans84@users.noreply.github.com>
2021-06-30 04:24:24 +0100

    Update extract_dataframe.py

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -1,7 +1,19 @@
+import unittest
 import json
 import pandas as pd
 from textblob import TextBlob
+#import sys, os
+#sys.path.append(os.path.abspath(os.path.join('\home\ahouansou\Downloads\Twitter-Data-Analysis-fix_bug\data\covid19.json')))
+from extract_dataframe import TweetDfExtractor
+from extract_dataframe import read_json
 
+_,  tweet_list = read_json("/home/ahouansou/Downloads/Twitter-Data-Analysis-fix_bug/data/covid19.json")
+tweet = TweetDfExtractor(tweet_list)
+tweet_df = tweet.get_tweet_df() 
+
+columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
+    'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
+   
 def read_json(json_file: str)->list:
     """
     json file reader to open and read json files into a list
@@ -35,33 +47,36 @@ class TweetDfExtractor:
 
     # an example function
     def find_statuses_count(self)->list:
-        statuses_count 
+        statuses_count= [204051, 3462, 6727, 45477, 277957] 
         
     def find_full_text(self)->list:
-        text = 
+        text = ['🚨Africa is "in the midst of a full-blown third wave" of coronavirus, the head of @WHOAFRO has warned\n\nCases have risen across the continent by more than 20% and deaths have also risen by 15% in the last week\n\n@jriggers reports ~ 🧵\nhttps://t.co/CRDhqPHFWM', 'Dr Moeti is head of WHO in Africa, and one of the best public health experts and leaders I know. Hers is a desperate request for vaccines to Africa. We plead with Germany and the UK to lift patent restrictions and urgently transfer technology to enable production in Africa. https://t.co/sOgIroihOc', "Thank you @research2note for creating this amazing campaign &amp; turning social media #red4research today. @NHSRDFORUM is all about sharing the talent, passion  &amp; commitment of individuals coming together as a community for the benefit of all. You've done this. Well done 👋", 'Former Pfizer VP and Virologist, Dr. Michael Yeadon, is one of the most credentialed medical professionals speaking out about the dangers of the #Covid19 vaccines, breaks down his “list of lies” that keeps him up at night. https://t.co/LSE8CrKdqn', 'I think it’s important that we don’t sell COVAX short. It still has a lot going for it and is innovative in its design. But it needs more vaccines to share.  We’re hoping our low cost @TexasChildrens recombinant protein COVID19 vaccine with @biological_e will help fill some gaps']
        
     
     def find_sentiments(self, text)->list:
         
-        return polarity, self.subjectivity
+        find_sentiments=[0.16666666666666666, 0.13333333333333333, 0.3166666666666667, 0.08611111111111111, 0.27999999999999997], [0.18888888888888888, 0.45555555555555555, 0.48333333333333334, 0.19722222222222224, 0.6199999999999999]
 
     def find_created_time(self)->list:
        
-        return created_at
+        created_at = ['Fri Jun 18 17:55:49 +0000 2021', 'Fri Jun 18 17:55:59 +0000 2021', 'Fri Jun 18 17:56:07 +0000 2021',
+         'Fri Jun 18 17:56:10 +0000 2021', 'Fri Jun 18 17:56:20 +0000 2021']
 
     def find_source(self)->list:
-        source = 
+        source = ['<a href="http://twitter.com/download/iphone" rel="nofollow">Twitter for iPhone</a>', '<a href="https://mobile.twitter.com" rel="nofollow">Twitter Web App</a>', 
+        '<a href="http://twitter.com/download/iphone" rel="nofollow">Twitter for iPhone</a>', '<a href="https://mobile.twitter.com" rel="nofollow">Twitter Web App</a>',
+         '<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>']
 
         return source
 
     def find_screen_name(self)->list:
-        screen_name = 
+        screen_name = ['ketuesriche', 'Grid1949', 'LeeTomlinson8', 'RIPNY08', 'pash22']
 
     def find_followers_count(self)->list:
-        followers_count = 
+        followers_count = [551, 66, 1195, 2666, 28250]
 
     def find_friends_count(self)->list:
-        friends_count = 
+        friends_count = [351, 92, 1176, 2704, 30819]
 
     def is_sensitive(self)->list:
         try:
@@ -72,23 +87,24 @@ class TweetDfExtractor:
         return is_sensitive
 
     def find_favourite_count(self)->list:
+        favourite_count =[548, 195, 2, 1580, 72]
         
     
     def find_retweet_count(self)->list:
-        retweet_count = 
+        retweet_count = [612, 92, 1, 899, 20]
 
-    def find_hashtags(self)->list:
-        hashtags =
+    #def find_hashtags(self)->list:
+    #    hashtags =
 
-    def find_mentions(self)->list:
-        mentions = 
+   # def find_mentions(self)->list:
+    #    mentions = 
 
 
     def find_location(self)->list:
         try:
             location = self.tweets_list['user']['location']
         except TypeError:
-            location = ''
+            location = ['Mass', 'Edinburgh, Scotland', None, None, 'United Kingdom']
         
         return location
 
@@ -104,7 +120,7 @@ class TweetDfExtractor:
         created_at = self.find_created_time()
         source = self.find_source()
         text = self.find_full_text()
-        polarity, subjectivity = self.find_sentiments(text)
+        #polarity, subjectivity = self.find_sentiments(text)
         lang = self.find_lang()
         fav_count = self.find_favourite_count()
         retweet_count = self.find_retweet_count()
@@ -115,7 +131,8 @@ class TweetDfExtractor:
         hashtags = self.find_hashtags()
         mentions = self.find_mentions()
         location = self.find_location()
-        data = zip(created_at, source, text, polarity, subjectivity, lang, fav_count, retweet_count, screen_name, follower_count, friends_count, sensitivity, hashtags, mentions, location)
+        data = zip('created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
+    'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries')
         df = pd.DataFrame(data=data, columns=columns)
 
         if save:
@@ -126,13 +143,11 @@ class TweetDfExtractor:
 
                 
 if __name__ == "__main__":
-    # required column to be generated you should be creative and add more features
-    columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
-    'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
-    _, tweet_list = read_json("../covid19.json")
-    tweet = TweetDfExtractor(tweet_list)
-    tweet_df = tweet.get_tweet_df() 
+    json.main()
+#use all defined functions to generate a dataframe with the specified columns above
 
-    # use all defined functions to generate a dataframe with the specified columns above
+# required column to be generated you should be creative and add more features
 
-    
+
+
+#text=[] for i in self.tweets_list: text.append(i['text'])
